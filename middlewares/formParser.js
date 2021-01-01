2641cf5cbb85f08fe068cfe7452964b5020d0542
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-01 17:32:15 -0500

    add form parser

---
+++ middlewares/formParser.js
@@ -0,0 +1,31 @@
+
+
+const formidable = require('formidable');
+
+const formParser = async (req,res,next) => {
+    const form = formidable({multiples:true})
+    const acceptedFormats = new Set(['image/jpeg','image/jpg','image/png']);
+    const parsedReq = await form.parse(req, (err,fields,files) => {
+        if (err){
+            return res.status(500).json({
+                error: "Couldn't parse the form. Try again later"
+            })
+        }
+
+        Object.keys(files).forEach(fileName => {
+            file = files[fileName]
+            if (!acceptedFormats.has(file.type)) {
+                return next(new Error('Unacceptable file format'))
+            }
+        })
+        
+        req.body = {...files,...fields,...req.body};
+        next();
+    })
+    if (parsedReq.type == 'json'){
+        next();
+    }
+
+}
+
+module.exports = formParser;
