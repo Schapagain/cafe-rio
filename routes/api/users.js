473e9970a5b2f76314efe6c6426dacbc98b31ac2
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-01 17:32:57 -0500

    refactor to controllers

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,65 +1,56 @@
 
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const { getAuthToken } = require('../../utils/authorization');
 
 // Import the user model
-const User = require('../../models/user');
-const { getCleanUsers } = require('../../utils/users');
+const { User } = require('../../database/models');
+const formParser = require('../../middlewares/formParser');
+const { signupUser, deleteUser } = require('../../controllers/users');
 
-// @route   POST api/users
-// @desc    Add a new user
-// @access  Public
-router.post('/', async (req,res) => {
-
-    let {name,email,password} = req.body;
-
-    // Check if all fields are given
-    if (!name || !email || !password){
-        return res.status(400).json({
-            error: "Please provide all required user properties"
-        })
-    }
-
+/**
+ * Route to sigup a user
+ * @name    api/users/signup
+ * @method  POST
+ * @access  Public
+ * @inner
+ * @param   {string} path
+ * @param   {callback} middleware - Form Parser  
+ * @param   {callback} middleware - Handle HTTP response
+*/
+router.post('/signup', formParser, async (req,res) => {
     try{
-        // Check if the user already exists
-        let oldUser = await User.findOne({ email })
-        if (oldUser) {
-            return res.status(400).json({
-                error: "Email has already been registered"
-            })
-        }
-
-        // Save user with a hashed password
-        password = await generatePasswordHash(password);
-        let newUser = new User({name,email,password});
-        let user = await newUser.save();
-
-        // Clean up User and assign token before sending the user back
-        const token = getAuthToken(user.id);
-        user = getCleanUsers(user).pop();
-        res.status(200).json({
-            user,
-            token,
-        })
+        const result = await signupUser(req.body);
+        res.status(200).json(result);
     }
     catch(err){
-        console.log(err);
-        return res.status(500).json({
-            error: 'Could not add a new user. Try again later.'
+        return res.status(err.httpCode || 500).json({
+            field: err.field,
+            error: err.message
         })
     }
 })
 
-const generatePasswordHash = async passwordPlain => {
+/**
+ * Route to delete a user
+ * @name    api/users
+ * @method  DELETE
+ * @access  Public
+ * @inner
+ * @param   {string} path
+ * @param   {callback} middleware - Form Parser  
+ * @param   {callback} middleware - Handle HTTP response
+*/
+router.delete('/', formParser, async (req,res) => {
     try{
-        const saltRounds = 5;
-        const passwordHash = await bcrypt.hash(passwordPlain, saltRounds);
-        return passwordHash;
+        const result = await deleteUser(req.body);
+        res.status(200).json(result);
     }
     catch(err){
-        throw err;
+        return res.status(500).json({
+            error: 'Could not delete user. Try again later.'
+        })
     }
-}
+})
+
 module.exports = router;
