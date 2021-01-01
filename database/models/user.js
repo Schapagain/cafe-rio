7e5cbe35ab4a01a5b2215ae2e48c4e4c6c6e74cb
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-01 17:31:44 -0500

    create a user model

---
+++ database/models/user.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Full name is required',
+        trim: true,
+        validate: {
+            validator: async function(name) {
+                return !name || (name.split(' ')).length > 1;
+            },
+            message: 'Full name is required'
+        }
+    },
+    organization: {
+        type: String,
+        required: "Organization is required",
+    },
+    employeeId: {
+        type: String,
+        required: "EmpoloyeeId is required",
+    },
+    phone: {
+        type: String,
+        required: "Phone is required",
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please fill a valid email address']
+    },
+    idCard: {
+        type: String,
+        required: "idCard is required"
+    },
+    password: {
+        type: String,
+        required: "Password is required"
+    }
+},{timestamps: true})
+
+/**
+ * Hash password before saving user
+ */
+UserSchema.pre('save',async function(next) {
+    let user = this;
+    if (!user.isModified('password')) return next();
+    user.password = generatePasswordHash(user.password);
+    return next();
+})
+
+/**
+ * Provide an API to compare passwords
+ * @param {String} candidatePassword 
+ */
+UserSchema.methods.validatePassword = async function(candidatePassword) {
+        return bcrypt.compare(candidatePassword, this.password);
+};
+
+/**
+ * Convert the given plaintext password to a hash
+ * @param {String} passwordPlain 
+ */
+async function generatePasswordHash(passwordPlain) {
+    try{
+        const saltRounds = 5;
+        const passwordHash = await bcrypt.hash(passwordPlain, saltRounds);
+        return passwordHash;
+    }
+    catch(err){
+        throw err;
+    }
+}
+
+module.exports = User = mongoose.model('users',UserSchema);
