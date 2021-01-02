19e30bdf4a5be5ec28c685e76af643ea1cbcb3f7
Desmond Tuiyot <dtuiyot@gmail.com>
2021-01-02 06:25:11 -0500

    changed auth.js password comparison since password isn't currently being hashed when signing up

--- controllers/users.js
+++ controllers/users.js
@@ -1,61 +1,57 @@
-
-const { getAuthToken } = require('../utils/authorization');
+const { getAuthToken } = require("../utils/authorization");
 
 // Import the user model
-const { User } = require('../database/models');
-const { getError, ValidationError } = require('./errors');
-const { saveFiles, deleteFiles } = require('./files');
+const { User } = require("../database/models");
+const { getError, ValidationError } = require("./errors");
+const { saveFiles, deleteFiles } = require("./files");
 
 /**
  * Save user info to the database
  * and user files into the disk
- * @param {*} user 
+ * @param {*} user
  */
 async function signupUser(user) {
+  let idCard, newUser, token;
+  try {
+    // check if idCard is sent
+    if (!user.idCard || !(typeof user.idCard == "object"))
+      throw new ValidationError("idCard", "Upload a picture of an Id card");
 
-    let idCard,newUser,token;
-    try {   
+    // save file and replace file with a random fileName
+    idCard = (await saveFiles([user.idCard]))[0];
+    user.idCard = idCard;
+    newUser = new User(user);
+    await newUser.save();
 
-        // check if idCard is sent
-        if (!user.idCard || !(typeof user.idCard == 'object')) throw new ValidationError('idCard','Upload a picture of an Id card')
-
-        // save file and replace file with a random fileName
-        idCard = (await saveFiles([user.idCard]))[0];
-        user.idCard = idCard;
-        newUser = new User(user);
-        await newUser.save();
-
-        token = getAuthToken(user.id);
-        user = {
-            ...user,
-            id: newUser._id,
-        }
-        return { user,token }
-    }
-    catch(err){
-        // delete saved file in case of error
-        idCard ? deleteFiles([idCard]) : null;
-        throw await getError(err);
-    }
-
+    token = getAuthToken(user.id);
+    user = {
+      ...user,
+      id: newUser._id,
+    };
+    return { user, token };
+  } catch (err) {
+    // delete saved file in case of error
+    idCard ? deleteFiles([idCard]) : null;
+    throw await getError(err);
+  }
 }
 
 /**
  * Remove user from the database
  * and user files from the disk
- * @param {*} user 
+ * @param {*} user
  */
 async function deleteUser(user) {
-    try{
-        const _id = user.id;
-        const result = await User.findOne({_id});
-        if (!result) return new Error('User not found')
-        await User.deleteOne({_id})
-        deleteFiles([result.idCard]);
-        return {id:user.id}
-    }catch(err){
-        throw await getError(err);
-    }
+  try {
+    const _id = user.id;
+    const result = await User.findOne({ _id });
+    if (!result) return new Error("User not found");
+    await User.deleteOne({ _id });
+    deleteFiles([result.idCard]);
+    return { id: user.id };
+  } catch (err) {
+    throw await getError(err);
+  }
 }
 
-module.exports = { signupUser, deleteUser }
+module.exports = { signupUser, deleteUser };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,58 +1,59 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { getAuthToken } = require('../../utils/authorization');
-const auth = require('../../middlewares/auth');
+const bcrypt = require("bcrypt");
+const { getAuthToken } = require("../../utils/authorization");
+const auth = require("../../middlewares/auth");
 
 // Import the user model
-const { User } = require('../../database/models');
+const { User } = require("../../database/models");
 
 // @route   POST api/auth
 // @desc    Authenticate user
 // @access  Public
-router.post('/', async (req,res) => {
+router.post("/", async (req, res) => {
+  let { email, password } = req.body;
+  // Check if all fields are given
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      error: "Please provide all required user properties",
+    });
+  }
 
-    let {email,password} = req.body;
-    // Check if all fields are given
-    if (!email || !password){
-        return res.status(400).json({
-            success: false,
-            error: "Please provide all required user properties"
-        })
+  try {
+    // Check if the user already exists
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        error: "User doesn't exist",
+      });
     }
 
-    try{
-        // Check if the user already exists
-        let user = await User.findOne({ email })
-        if (!user) {
-            return res.status(400).json({
-                error: "User doesn't exist"
-            })
-        } 
-
-        // Compare username/password combination
-        const credentialsMatch = await bcrypt.compare(password,user.password);
-        if (!credentialsMatch) return res.status(401).json({error:"Unauthorized"})
-
-        // Clean up User and assign token before sending the user back
-        const token = getAuthToken(user.id);
-        res.status(200).json({
-            user,
-            token,
-        })
+    // Compare username/password combination
+    if (password !== user.password) {
+      return res.status(401).json({ error: "Unauthorized" });
     }
-    catch(err){
-       res.status(err.httpCode || 500).json({error: err.message}) 
-    }
-})
+    // const credentialsMatch = await bcrypt.compare(password, user.password);
+    // if (!credentialsMatch)
+    //   return res.status(401).json({ error: "Unauthorized" });
+
+    // Clean up User and assign token before sending the user back
+    const token = getAuthToken(user.id);
+    res.status(200).json({
+      user,
+      token,
+    });
+  } catch (err) {
+    res.status(err.httpCode || 500).json({ error: err.message });
+  }
+});
 
 // @route   GET api/auth/user
 // @desc    Get user data
 // @access  Private
-router.get('/user', auth, async (req,res) => {
-    const {name,email} = await User.findById(req.id);
-    res.status(200).json({name,email});
-})
+router.get("/user", auth, async (req, res) => {
+  const { name, email } = await User.findById(req.id);
+  res.status(200).json({ name, email });
+});
 
 module.exports = router;
