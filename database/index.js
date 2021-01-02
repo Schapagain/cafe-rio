15d9bcdec8f6254d9741db24d2f30cbd2477d794
Desmond Tuiyot <dtuiyot@gmail.com>
2021-01-02 05:16:09 -0500

    fixed DeprecationWarning - useCreateIndexes

--- database/index.js
+++ database/index.js
@@ -1,7 +1,13 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 const mongoURI = `mongodb+srv://sandesh:${process.env.DBPASSWORD}@mongo-aws.bav9k.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 mongoose
-    .connect(mongoURI,{ useNewUrlParser: true,useUnifiedTopology: true })
-    .then(() => {console.log('Connected to MongoDB')})
-    .catch( err => console.log(err))
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
