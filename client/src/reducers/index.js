0b4458b1faf2be3e9d107a66fc31a94aa5e69cb4
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-03 12:06:22 -0500

    remove client side

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import errorReducer from "./error_reducer";
-import authReducer from "./auth_reducer";
-
-export default combineReducers({
-  error: errorReducer,
-  auth: authReducer,
-});
