6e7fbfbd4b7d47b14111e0c66badba50c6c12e57
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-03 10:14:50 -0500

    add routes to get user details

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getAuthToken } = require("../../utils/authorization");
+
 
 // Import the user model
 const { User } = require("../../database/models");
 const formParser = require("../../middlewares/formParser");
-const { signupUser, deleteUser } = require("../../controllers/users");
+const { signupUser, deleteUser, getUsers } = require("../../controllers/users");
 
 /**
- * Route to fetch all user info
+ * Route to fetch all users
  * @name api/users
  * @method POST
  * @access Public
@@ -18,7 +18,18 @@ const { signupUser, deleteUser } = require("../../controllers/users");
  * @param   {callback} middleware - Handle HTTP response
  */
 router.get("/", formParser, async (req, res) => {
-  User.find().then((items) => res.json(items));
+ 
+  try{
+    const result = await getUsers();
+    res.status(200).json(result);
+  }catch(err) {
+    res.status(httpCode || 500).json({
+      error: {
+        msg: err.message
+      }
+    })
+  }
+  
 });
 
 /**
@@ -37,9 +48,32 @@ router.post("/signup", formParser, async (req, res) => {
     res.status(200).json(result);
   } catch (err) {
     return res.status(err.httpCode || 500).json({
-      field: err.field,
-      error: err.message,
+      error: {
+        field: err.field,
+        msg: err.message
+      }
     });
+  }
+});
+
+/**
+ * Route to fetch user details
+ * @name api/users/:id
+ * @method POST
+ * @access Public
+ * @inner
+ * @param {string} path
+ * @param {callback} middleware - Form Parser
+ * @param   {callback} middleware - Handle HTTP response
+ */
+router.get("/:id", formParser, async (req, res) => {
+  try{
+    const result = await getUsers(req.params.id);
+    res.status(200).json(result);
+  }catch(err){
+    return res.status(err.httpCode || 500).json({
+      error: err.message
+    })
   }
 });
 
