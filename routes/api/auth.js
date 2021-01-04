75e8fa6cdd2a335ff161279e69a29512fa5c8966
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-04 16:16:33 -0500

    add route to allow admin log in

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const formParser = require("../../middlewares/formParser");
-const { authenticate, activateAccount } = require('../../controllers/auth');
+const { authenticate, activateAccount, authenticateAdmin } = require('../../controllers/auth');
 const appAddress = 'https://cafe-rio.netlify.app/activate'
 
 /**
@@ -19,6 +19,26 @@ router.get("/activate/:activationCode", async (req, res) => {
   try {
     await activateAccount(req.params.activationCode);
     res.redirect(appAddress);
+  } catch (err) {
+    res.status(err.httpCode || 500).json({ error: err.message });
+  }
+});
+
+/**
+ * Route to authenticate admin credentials
+ * @name api/auth/admin
+ * @method POST
+ * @access Public
+ * @inner
+ * @param {string} path
+ * @param {callback} middleware - Form Parser
+ * @param   {callback} middleware - Handle HTTP response
+ */
+router.post("/admin", formParser, async (req, res) => {
+
+  try {
+    const result = await authenticateAdmin(req.body);
+    res.status(200).json(result);
   } catch (err) {
     res.status(err.httpCode || 500).json({ error: err.message });
   }
