4d87798b5cebbb25629cde8cc825f24cc4b5dc10
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-05 09:14:07 -0500

    refactor

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -15,39 +15,70 @@ function auth(allowedRoles) {
     return async function (req,res,next) {
         
         try{
-            // Get token from the header
-            const [authMethod,userToken] = req.header('authorization') 
-            ? req.header('authorization').split(' ') 
-            : ['',''];
-
-            // ensure existence of auth method and token in correct format
-            if (!userToken && !authMethod) throw new ValidationError('token')
-            const methodIsValid = getValidAuthMethods().includes(authMethod.toLowerCase());
-            if (!authMethod || !methodIsValid) throw new ValidationError('token','auth format not supported');
-            if (!userToken) throw new ValidationError('token');
-
-            // Verify token and extract user id
-            const token = njwt.verify(userToken,signingKey);
-            const tokenId = token.body.sub;
-            const tokenRole = token.body.scope;
-
-            if (!allowedRoles.includes(tokenRole))
-                throw new NotAuthorizedError('No access');
-
-            // Restrict cross user access
-            if (tokenRole != ADMIN && req.params.id && (tokenId != req.params.id))
-                throw new NotAuthorizedError('Private route');
+            const token = parseAuthHeader(req.header('authorization'));
+            const {id,role} = parseToken(token,signingKey);
+            verifyRoutePermission(allowedRoles,role);
+            if (role !== ADMIN)
+                verifyRouteOwnership(req.params.id,id);
 
             // Inject userId into req before proceeding
-            req.auth = {
-                id: tokenId,
-                role: tokenRole
-            }
+            req.auth = {id,role}
             next();
         }catch(err){
             next(await getError(err))
         }
     }
+}
+
+/**
+ * Verify the given token, and return owner's id, role
+ * @param {String} userToken 
+ * @param {String} signingKey 
+ */
+function parseToken(userToken,signingKey) {
+    const token = njwt.verify(userToken,signingKey);
+    const id = token.body.sub;
+    const role = token.body.scope;
+    return {id,role}
+}
+
+/**
+ * Ensure that users can only access their own resources 
+ * @param {String} ownerId 
+ * @param {String} id 
+ */
+function verifyRouteOwnership(ownerId,id) {
+    if (ownerId && id != ownerId)
+        throw new NotAuthorizedError('Private route')
+}
+
+/**
+ * Ensure that the role of the current user has permissions to this route
+ * @param {String[]} allowedRoles 
+ * @param {String} role 
+ */
+function verifyRoutePermission(allowedRoles,role) {
+    if (!allowedRoles.includes(role))
+        throw new NotAuthorizedError('No access');
+}
+
+/**
+ * Extracts user token from Authorization header
+ * @param {String} authHeader 
+ */
+function parseAuthHeader(authHeader) {
+
+    const [authMethod,userToken] = authHeader
+    ? authHeader.split(' ') 
+    : ['',''];
+
+    // ensure existence of auth method and token in correct format
+    if (!userToken && !authMethod) throw new ValidationError('token')
+    const methodIsValid = getValidAuthMethods().includes(authMethod.toLowerCase());
+    if (!authMethod || !methodIsValid) throw new ValidationError('token','auth format not supported');
+    if (!userToken) throw new ValidationError('token');
+
+    return userToken;
 }
 
 module.exports = auth;
