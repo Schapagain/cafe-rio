bbcd8d6d31ec9ed237b0b08615663c046ddb2684
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-05 08:23:48 -0500

    replace custom cors code block with express cors library

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
-
-// random comment
-
-
+// middlewares for CORS and json parsing
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// Enable Cross Origin Sharing for everyone
-app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','*');
-
-    // Handle initial OPTIONS request
-    if (req.method === "OPTIONS") {
-        res.header('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE');
-        return res.status(200).json({});
-    }
-    next();
-});
 
 // Serve API routes
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'))
 app.use('/api/meals',require('./routes/api/meals'));
-
 
 // Serve static content in production
 if(process.env.NODE_ENV == "production") {
