adc155204040c6c1cb4c225d59850dd1ff09dbde
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-05 08:25:10 -0500

    check for proper bearer token format

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,16 +2,29 @@ const njwt = require('njwt');
 require('dotenv').config();
 const signingKey = process.env.SECRET_KEY;
 const { getError, ValidationError, NotAuthorizedError } = require('../controllers/errors');
+const { getValidAuthMethods } = require('../controllers/auth');
 
 const { ADMIN } = require('../controllers/roles');
 
+/**
+ * Verify that the role and id associated with the token 
+ * is allowed to access the attempted route
+ * @param {String[]} allowedRoles 
+ */
 function auth(allowedRoles) {
     return async function (req,res,next) {
         
         try{
             // Get token from the header
-            const userToken = req.header('authorization');
-            if (!userToken) throw new ValidationError('token')
+            const [authMethod,userToken] = req.header('authorization') 
+            ? req.header('authorization').split(' ') 
+            : ['',''];
+
+            // ensure existence of auth method and token in correct format
+            if (!userToken && !authMethod) throw new ValidationError('token')
+            const methodIsValid = getValidAuthMethods().includes(authMethod.toLowerCase());
+            if (!authMethod || !methodIsValid) throw new ValidationError('token','auth format not supported');
+            if (!userToken) throw new ValidationError('token');
 
             // Verify token and extract user id
             const token = njwt.verify(userToken,signingKey);
@@ -30,7 +43,6 @@ function auth(allowedRoles) {
                 id: tokenId,
                 role: tokenRole
             }
-
             next();
         }catch(err){
             next(await getError(err))
