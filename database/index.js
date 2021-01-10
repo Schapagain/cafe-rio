35dc8f1c810ab435eaaaaf78b274623ecb9e38ce
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-09 20:22:25 -0500

    refactor querying database

--- database/index.js
+++ database/index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { ValidationError } = require("../controllers/errors");
 require("dotenv").config();
 const mongoURI = `mongodb+srv://sandesh:${process.env.DBPASSWORD}@mongo-aws.bav9k.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 mongoose
@@ -12,6 +13,17 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+const isValidMongooseId = id => mongoose.Types.ObjectId.isValid(id);
+
+const queryDatabase = async ({model,query,attributes}) => {
+  if (query && query.id && !isValidMongooseId(query.id)) {
+    throw new ValidationError('id');
+  }
+
+  return model.find(query,'-_id '.concat(attributes.join(' ')));
+}
+
 module.exports = {
-  isValidMongooseId: id => mongoose.Types.ObjectId.isValid(id)
+  isValidMongooseId,
+  queryDatabase,
 }
