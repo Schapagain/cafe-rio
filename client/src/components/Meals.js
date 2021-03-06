75b662023417f7032a752e4882c939c9ee67133a
Sandesh <chapagainsandesh88@gmail.com>
2021-03-05 21:19:28 -0500

    switch mui with tailwind

--- client/src/components/Meals.js
+++ client/src/components/Meals.js
@@ -1,57 +1,43 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import SingleMeal from "./SingleMeal";
-import Typography from "@material-ui/core/Typography";
 import { addMealToOrder } from "../actions/orderActions";
 // import Spinner from "./Spinner";
 import Skeleton from "@material-ui/lab/Skeleton";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    // maxWidth: "100%",
-  },
-  todaysMenuText: {
-    marginTop: "1em"
-  }
-}));
 
 const Meals = ({ meal, filter, addMealToOrder }) => {
-  const classes = useStyles();
 
   let { meals, isLoading } = meal;
   meals = meals.filter((meal) => meal.category === filter.toLowerCase());
   return (
-    // <Container maxWidth="md">
-    <>
-      <Typography variant="h5" className={classes.todaysMenuText}>
+    <div className="flex flex-col h-full sm:h-96">
+      <h1 className="font-medium w-1/2 text-black mx-auto bg-opacity-20 bg-gray-200 rounded-xl text-center text-3xl p-2 mt-10">
         {meals.length ? filter : null}
-      </Typography>
-      <Grid direction="row" container spacing={2} className={classes.root}>
-        {(isLoading ? Array.from(new Array(4)) : meals).map((meal, index) => (
-          <Grid item key={index} xs={12} sm={4}>
-            {meal ? (
-              <SingleMeal
-                meal={meal}
-                handleOnClick={(meal) => {
-                  addMealToOrder(meal);
-                }}
-              />
-            ) : (
-              <Skeleton
-                animation="wave"
-                variant="rect"
-                width={300}
-                height={200}
-              />
-            )}
-          </Grid>
-        ))}
-      </Grid>
-    </>
+      </h1>
+      <div className="flex w-full mx-auto flex-col overflow-y-auto sm:flex-row sm:overflow-x-auto">
+        {(isLoading 
+        ? Array.from(new Array(4)) 
+        : meals).map((meal, index) => meal 
+          ? ( <SingleMeal
+                  meal={meal}
+                  addToOrder={(meal) => {
+                    addMealToOrder(meal);
+                  }}
+                />) 
+          : ( <Skeleton
+                  animation="wave"
+                  variant="rect"
+                  width={300}
+                  height={200}
+                />
+            )
+          )
+        }
+      </div>
+    </div>
   );
 };
 
