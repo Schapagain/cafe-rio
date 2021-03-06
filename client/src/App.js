82633fa9164fed7506c8539a0f4f43faef082bfe
Sandesh <chapagainsandesh88@gmail.com>
2021-03-05 21:17:10 -0500

    refactor to a seperate container

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { Provider } from "react-redux";
-import { Switch, Route } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/styles";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-import NavBar from "./components/NavBar";
-import SignUp from "./components/SignUp";
-import SignIn from "./components/SignIn";
-import HomePage from "./components/HomePage";
-import Checkout from "./components/Checkout";
 import store from "./store";
 import { loadUser } from "./actions/authActions";
-import theme from "./components/Theme";
+import MainContainer from './components/MainContainer';
 
 const stripePromise = loadStripe(
   "pk_test_51I8QurI5qzReycR9FSYytJ8iPoxfXFQwYObAZC0x8jXsHY6K6XOCZ8AtF6N8NDiDpPC58I090d88Q0i0Y4PudyZo00AFJEaifB"
@@ -28,27 +20,11 @@ function App() {
   return (
     <Provider store={store}>
       <Elements stripe={stripePromise}>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <ThemeProvider theme={theme}>
-            <NavBar />
-            <Switch>
-              <Route
-                exact
-                path="/login"
-                render={(props) => <SignIn {...props.location.state} />}
-              />
-              <Route exact path="/signup">
-                <SignUp />
-              </Route>
-              <Route exact path="/checkout">
-                <Checkout />
-              </Route>
-              <Route path="/">
-                <HomePage />
-              </Route>
-            </Switch>
-          </ThemeProvider>
-        </MuiPickersUtilsProvider>
+        <Router>
+          <div className="w-full flex min-h-screen">
+            <MainContainer />
+          </div>
+        </Router>
       </Elements>
     </Provider>
   );
