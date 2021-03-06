a3e9e0c7d52016d2f35c294bf7ac93a73af70b9f
Sandesh <chapagainsandesh88@gmail.com>
2021-03-05 21:20:20 -0500

    add a new button component

---
+++ client/src/components/Button.js
@@ -0,0 +1,25 @@
+import classNames from 'classnames';
+
+const Button = ({text,icon,className,onClick,href}) => {
+    const mainClass = classNames(
+        'hover:shadow-2xl hover:scale-110',
+        className,
+        `flex bg-gray-300 text-white bg-gray-600 rounded-xl p-3 mx-auto m-3
+        transition duration-500 transform ease-in-out box-shadow`)
+    return (
+        <a 
+        className = {mainClass}
+        href = {href}
+        onClick={(e)=>{e.preventDefault();onClick()}} 
+        >
+            {icon && <p className="mx-2 my-auto">{icon}</p>}<p>{text}</p>
+        </a>
+    )
+}
+
+Button.defaultProps = {
+    onClick: () => {},
+    href: "!#"
+}
+
+export default Button;
