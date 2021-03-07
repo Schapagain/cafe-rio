2f6206f6c6b703a805d575fb313a5099cf0b6f96
Sandesh <chapagainsandesh88@gmail.com>
2021-03-06 23:04:26 -0500

    remove mui elements

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,166 +1,52 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
-import Drawer from "@material-ui/core/Drawer";
+import Drawer from "./Drawer";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import Badge from "@material-ui/core/Badge";
 import IconButton from "@material-ui/core/IconButton";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import { makeStyles } from "@material-ui/core/styles";
-import { connect } from "react-redux";
 
 import CartSingleMeal from "./CartSingleMeal";
 import emptyCartImage from "../images/empty_cart.png";
-import { Typography } from "@material-ui/core";
 import { addMealToOrder, removeMealFromOrder } from "../actions/orderActions";
 import { createPaymentIntent } from "../actions/paymentActions";
-import store from "../store";
 import { LOAD_CHECKOUT } from "../actions/types";
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
-const useStyles = makeStyles(() => ({
-  root: {
-    width: "25em",
-  },
-  shoppingCart: {
-    color: "white",
-  },
-  yourOrderContainer: {
-    alignSelf: "flex-start",
-  },
-  yourOrder: {
-    paddingLeft: ".8rem",
-    fontSize: "1.5rem",
-    fontWeight: "bolder",
-  },
-  checkoutButtonContainer: {
-    justifyContent: "center",
-  },
-  checkoutButton: {
-    width: "100%",
-    textAlign: "left",
-  },
-  checkoutText: {
-    marginRight: "auto",
-    fontSize: "1rem",
-  },
-  subPriceText: {
-    fontSize: "0.8rem",
-  },
-  emptyCart: {
-    marginTop: "100%",
-  },
-}));
+export const Cart = ({ order, className }) => {
+  const numItems = order.totalMeals;
 
-const Cart = ({ order, createPaymentIntent, removeMealFromOrder }) => {
-  const classes = useStyles();
-  const [openDrawer, setOpenDrawer] = useState(false);
-
-  const handleCheckout = async () => {
-    setOpenDrawer(false);
-    store.dispatch({ type: LOAD_CHECKOUT });
-    createPaymentIntent();
-  };
-
-  const [mealsOrdered, totalPrice, totalMeals] = [
-    order.meals,
-    order.totalPrice,
-    order.totalMeals,
-  ];
-
-  const emptyCart = (
-    <Grid className={classes.emptyCart} item xs={10}>
-      <img
-        className={classes.emptyCartImage}
-        src={emptyCartImage}
-        alt="emtpy cart"
-      />
-    </Grid>
-  );
-
-  const cartWithItems = (
-    <>
-      <Grid item xs={12} className={classes.yourOrderContainer}>
-        <Typography className={classes.yourOrder}>Your Order</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Divider />
-      </Grid>
-      <Grid item xs={11} className={classes.checkoutButtonContainer}>
-        <Button
-          variant="contained"
-          color="secondary"
-          className={classes.checkoutButton}
-          size="large"
-          onClick={handleCheckout}
-          component={Link}
-          to="/checkout"
-        >
-          <Typography variant="button" className={classes.checkoutText}>
-            CHECKOUT
-          </Typography>
-          <Typography variant="subtitle2" className={classes.subPriceText}>
-            ${totalPrice}
-          </Typography>
-        </Button>
-      </Grid>
-      {Array.from(mealsOrdered.keys()).map((mealId) => (
-        <CartSingleMeal
-          key={mealId}
-          meal={mealsOrdered.get(mealId).meal}
-          quantity={mealsOrdered.get(mealId).quantity}
-          handleRemove={() => {
-            removeMealFromOrder(mealId);
-          }}
-        />
-      ))}
-    </>
-  );
+  const [cartOpen,setCartOpen] = useState(false);
 
   return (
     <>
-      <IconButton
-        className={classes.shoppingCart}
-        aria-label="open shopping cart"
-        onClick={() => {
-          setOpenDrawer(!openDrawer);
-        }}
-        disableRipple
-        disableFocusRipple
+    <div 
+    className={`${className} h-full flex text-black hover:text-white hover:bg-theme-color rounded-xl select-none cursor-pointer relative w-full`}>
+      <AiOutlineShoppingCart 
+      className="m-1 text-4xl inline" 
+      onClick = {()=>setCartOpen(!cartOpen)}
+      />
+      <div 
+      className="absolute text-white top-0 right-0 bg-theme-color h-5 w-5 flex items-center text-xs justify-center p-3 rounded-full"
       >
-        <Badge badgeContent={totalMeals} color="secondary">
-          <ShoppingCartIcon />
-        </Badge>
-      </IconButton>
-      <Drawer
-        anchor="right"
-        open={openDrawer}
-        onClose={() => {
-          setOpenDrawer(false);
-        }}
-      >
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          spacing={1}
-          className={classes.root}
-        >
-          {totalMeals ? cartWithItems : emptyCart}
-        </Grid>
-      </Drawer>
+        {numItems || 0}
+      </div>
+    </div>
+    <Drawer toggleDrawer={()=>setCartOpen(!cartOpen)} cartOpen={cartOpen} />
     </>
-  );
-};
+  )
+}
+
+Cart.propTypes = {
+  order: PropTypes.object.isRequired
+}
 
 const mapStateToProps = (state) => ({
-  order: state.order.order,
-  meals: state.meal.meals,
-});
+  order: state.order.order
+})
 
-export default connect(mapStateToProps, {
-  addMealToOrder,
-  removeMealFromOrder,
-  createPaymentIntent,
-  store,
-})(Cart);
+
+export default connect(mapStateToProps, {})(Cart)
