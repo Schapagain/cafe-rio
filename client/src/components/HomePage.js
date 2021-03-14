618da391ec70857ed936ba729c13594af4c1a19c
Sandesh <chapagainsandesh88@gmail.com>
2021-03-14 10:34:42 -0400

    animated intro text

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,35 +1,45 @@
 import React from "react";
-import Button from './Button';
+import Button from "./Button";
 import { useHistory } from "react-router-dom";
+import { useSpring, animated } from "react-spring";
 
 const IntroText = () => {
-    return(
-        <div className="z-50 text-center text-white">
-            <p className="text-3xl sm:text-4xl lg:text-5xl mb-4">A place to recharge yourself</p>
-            <div className="text-xl sm:text-2xl lg:text-3xl">
-                <p>Drink some coffee.</p>
-                <p>Grab a snack.</p>    
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="z-50 text-center text-white">
+      <p className="text-3xl sm:text-4xl lg:text-5xl mb-4">
+        A place to recharge yourself
+      </p>
+      <div className="text-xl sm:text-2xl lg:text-3xl">
+        <p>Drink some coffee.</p>
+        <p>Grab a snack.</p>
+      </div>
+    </div>
+  );
+};
 
 const HomePage = () => {
+  const history = useHistory();
 
-    const history = useHistory();
+  const animation = useSpring({
+    to: { transform: "translateY(0%)" },
+    from: { transform: "translateY(-5%)" },
+  });
 
-    return (
-        <div className="flex justify-center lg:justify-start mx-auto w-full max-w-screen-xl h-2/3 my-auto">
-            <div className="flex lg:ml-20 flex-col my-auto ">
-                <IntroText />
-                <Button 
-                className="rounded-full hover:bg-theme-color"
-                text="See menu" 
-                onClick={()=>history.push("/menu")}
-                />    
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center lg:justify-start mx-auto w-full max-w-screen-xl h-2/3 my-auto">
+      <animated.div
+        style={animation}
+        className="flex lg:ml-20 flex-col my-auto"
+      >
+        <IntroText />
+        <Button
+          className="rounded-full hover:bg-theme-color"
+          text="See menu"
+          onClick={() => history.push("/menu")}
+        />
+      </animated.div>
+    </div>
+  );
+};
 
 export default HomePage;
