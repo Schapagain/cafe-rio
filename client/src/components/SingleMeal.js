6f83767acc2ccbd1f368f4ccfc1f7c965954663a
Sandesh <chapagainsandesh88@gmail.com>
2021-03-14 10:35:07 -0400

    adjust styles

--- client/src/components/SingleMeal.js
+++ client/src/components/SingleMeal.js
@@ -1,38 +1,36 @@
 import React from "react";
-import { MdAddShoppingCart } from 'react-icons/md';
+import { MdAddShoppingCart } from "react-icons/md";
 
-function Image({url}) {
+function Image({ url }) {
   return (
-      <img 
-      className="cursor-pointer rounded-xl w-full max-h-80 h-full object-cover" 
-      src={url} 
-      alt="cafe food option" /> 
-  )
+    <img
+      className="cursor-pointer rounded-xl w-full max-h-80 h-full object-cover"
+      src={url}
+      alt="cafe food option"
+    />
+  );
 }
 
-export default function SingleMeal({meal,addToOrder}) {
-  const {name,picture,price} = meal;
+export default function SingleMeal({ meal, addToOrder }) {
+  const { name, picture, price } = meal;
   return (
-    <div className="w-52 h-80 my-auto mx-2 p-2 flex flex-col">
-      <div 
-      onClick={()=>addToOrder(meal)} 
-      className="h-2/3 max-w-sm bg-theme-color rounded-xl flex relative"
+    <div className=" my-2 w-full cursor-pointer flex flex-col">
+      <div
+        onClick={() => addToOrder(meal)}
+        className="h-full w-full rounded-xl flex relative"
       >
-        <div 
-        className="h-full w-full z-30 transition duration-500 ease-in-out hover:opacity-50"
-        >
-          <Image url={picture}/>  
+        <div className="h-full p-1 flex w-full transition duration-500 ease-in-out hover:opacity-50">
+          <div className="flex w-full my-auto justify-between">
+            <p className="text-2xl">{name}</p>
+            <p className="my-auto text-xl">
+              ${(price && Number(meal.price).toFixed(2)) || "inf"}
+            </p>
+          </div>
         </div>
-        <div className="z-1 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <MdAddShoppingCart className="my-auto p-1 rounded-full text-6xl"/>
-        </div>
-      </div>
-      <div className="justify-between mt-2 mx-auto flex">
-        <div className="flex my-auto text-white bg-theme-color p-1 rounded-bl-xl rounded-tr-xl">
-          <p className="text-xl">{name}</p>
-          <p className="ml-2 my-auto">${(price && Number(meal.price).toFixed(2)) || 'inf'}</p>   
+        <div className="  p-2 rounded-2xl bg-theme-color mx-auto w-full z-30 opacity-0 hover:opacity-70 transition duration-500 ease-in-out absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <MdAddShoppingCart className="m-auto rounded-full text-3xl" />
         </div>
       </div>
     </div>
-  )
+  );
 }
