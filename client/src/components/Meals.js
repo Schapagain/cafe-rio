12beb089fa7f2381ab0818065e6c602cf46da298
Sandesh <chapagainsandesh88@gmail.com>
2021-03-14 10:36:59 -0400

    refactor and adjust design

--- client/src/components/Meals.js
+++ client/src/components/Meals.js
@@ -1,51 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import SingleMeal from "./SingleMeal";
 import { addMealToOrder } from "../actions/orderActions";
 import Skeleton from "@material-ui/lab/Skeleton";
+import { MdUnfoldMore } from "react-icons/md";
 
+const MAX_DISPLAY_COUNT_DEFAULT = 5;
 
-const Meals = ({ meal, filter, addMealToOrder }) => {
+const FilterDisplayImage = ({ className, images }) => {
+  return (
+    <div className={className + " p-5 justify-center flex h-96"}>
+      <img
+        className="w-full rounded-xl object-cover h-full"
+        src={images[1] || images[0]}
+        alt="food or drink"
+      />
+    </div>
+  );
+};
 
+const FilterMenu = ({
+  meals,
+  isLoading,
+  filter,
+  className,
+  addMealToOrder,
+  displayCount,
+}) => {
+  return (
+    <div className={className + " flex w-full p-5 flex-col"}>
+      <h2 className="text-4xl">{filter}</h2>
+      <div className="flex flex-col w-full justify-items-start justify-start h-full">
+        {(isLoading ? Array.from(new Array(3)) : meals)
+          .slice(0, displayCount)
+          .map((meal, index) =>
+            meal ? (
+              <SingleMeal
+                key={meal.id || index}
+                meal={meal}
+                addToOrder={(meal) => {
+                  addMealToOrder(meal);
+                }}
+              />
+            ) : (
+              <Skeleton
+                key={index}
+                animation="wave"
+                variant="rect"
+                width={300}
+                height={100}
+              />
+            )
+          )}
+      </div>
+    </div>
+  );
+};
+
+const Meals = ({ meal, filter, flexOrder, addMealToOrder }) => {
   let { meals, isLoading } = meal;
   meals = meals.filter((meal) => meal.category === filter.toLowerCase());
-  return (
-    <div className="flex w-full my-10 flex-col h-full">
-      <h1 className="font-medium w-1/2 text-black mx-auto bg-opacity-20 bg-gray-200 rounded-xl text-center text-3xl p-2 mb-5 mt-5">
-        {filter}
-      </h1>
-      {(isLoading || meals.length) 
-      ? (<div className="flex w-full justify-center h-full flex-wrap">
-        {(isLoading 
-        ? Array.from(new Array(4)) 
-        : meals).map((meal, index) => meal 
-          ? ( <SingleMeal
-                  key={meal.id || index}
-                  meal={meal}
-                  addToOrder={(meal) => {
-                    addMealToOrder(meal);
-                  }}
-                />) 
-          : ( <Skeleton
-                  key={index}
-                  animation="wave"
-                  variant="rect"
-                  width={300}
-                  height={200}
-                />
-            )
-          )
-        }
+  const [toggled, setToggled] = useState(false);
+  return isLoading || meals.length ? (
+    <div
+      className={`flex w-full mb-10 h-full  ${
+        flexOrder ? "flex-row-reverse mr-10" : "flex-row ml-10"
+      }`}
+    >
+      <FilterDisplayImage
+        className="w-1/2"
+        images={meals.map((meal) => meal.picture)}
+      />
+      <FilterMenu
+        className="w-1/4"
+        filter={filter}
+        isLoading={isLoading}
+        meals={meals}
+        displayCount={toggled ? meals.length : MAX_DISPLAY_COUNT_DEFAULT}
+        addMealToOrder={addMealToOrder}
+      />
+      <div className="w-1/4 flex">
+        {MAX_DISPLAY_COUNT_DEFAULT < meals.length && (
+          <MdUnfoldMore
+            onClick={() => setToggled(!toggled)}
+            className="m-auto p-2 rounded-full bg-theme-color cursor-pointer text-6xl"
+          />
+        )}
       </div>
-      ) : (
-        <div className="h-96 flex text-black text-xl sm:text-2xl">
-          <h1 className="m-auto bg-red-300 p-3 rounded-lg">Sorry, couldn't get {filter ? filter.toLowerCase() : "meal"} right now</h1>
-        </div>
-      )}
     </div>
-    );
+  ) : null;
 };
 
 Meals.propTypes = {
