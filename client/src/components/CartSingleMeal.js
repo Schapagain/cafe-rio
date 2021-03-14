bb6dead74448bd42df569bdeb04924b9ac907ea7
Sandesh <chapagainsandesh88@gmail.com>
2021-03-14 10:34:26 -0400

    create a drawer

--- client/src/components/CartSingleMeal.js
+++ client/src/components/CartSingleMeal.js
@@ -1,15 +1,30 @@
 import React from "react";
 
-const CartSingleMeal = ({ meal, quantity, handleRemove }) => {
+const CartSingleMeal = ({
+  meal,
+  quantity,
+  handleAddOne,
+  handleSubtractOne,
+}) => {
   return (
-    <div className="flex h-16 w-full p-4 justify-between">
+    <div className="flex h-16 w-full justify-between">
       <div className="w-3/4 my-auto flex">
         <p className="mr-2 my-auto">{meal.name}</p>
-        <p className="mx-1 my-auto rounded-full w-4 flex items-center justify-center h-4 bg-theme-color">-</p>
-        <p className="my-auto">{quantity}</p>
-        <p className="mx-1 my-auto rounded-full w-4 flex items-center justify-center h-4 bg-theme-color">+</p>
+        <p
+          onClick={() => handleSubtractOne(meal.id)}
+          className="p-2 mx-1 bg-gray-600 bg-opacity-50 my-auto rounded-full w-6 h-6 flex items-center justify-center  bg-theme-color"
+        >
+          -
+        </p>
+        <p className="my-auto p-2 ">{quantity}</p>
+        <p
+          onClick={() => handleAddOne(meal)}
+          className="mx-1 p-2 bg-gray-600 bg-opacity-50 my-auto rounded-full w-6 h-6 flex items-center justify-center  bg-theme-color"
+        >
+          +
+        </p>
       </div>
-      <div className="my-auto">
+      <div className="my-auto mr-1">
         <p>${(meal.price || 99999) * quantity}</p>
       </div>
     </div>
--- client/src/components/Drawer.js
+++ client/src/components/Drawer.js
@@ -1,60 +1,126 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { useSpring, a, config } from 'react-spring';
-import CartSingleMeal from './CartSingleMeal';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { useTransition, animated } from "react-spring";
+import Button from "./Button";
+import CartSingleMeal from "./CartSingleMeal";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { addMealToOrder, removeMealFromOrder } from "../actions/orderActions";
 
-export const Drawer = ({ cartOpen, toggleDrawer, order }) => {
-    const orderMap = (order && order.meals) || new Map();
-    const mealsInOrder = Array.from(orderMap.values());
+const Leaf = ({ cartOpen, setCartOpen, numItems }) => {
+  const transitions = useTransition(cartOpen, null, {
+    from: { transform: "translate3d(-250%,0,0)" },
+    enter: { transform: "translate3d(50%,0,0)" },
+    leave: { transform: "translate3d(-250%,0,0)" },
+    reverse: cartOpen,
+    unique: true,
+    reset: true,
+    initial: { transform: "translate3d(100%,0,0)" },
+  });
+  return (
+    <>
+      {transitions.map(({ key, props }) => (
+        <animated.div
+          key={key}
+          style={props}
+          onClick={() => setCartOpen(!cartOpen)}
+          className="bg-theme-color text-offwhite cursor-pointer z-40 fixed rounded-full top-1/2 transform -translate-y-1/2 right-0 w-32 h-32"
+        >
+          <div className="absolute left-1/2 transform -translate-y-1/2 -translate-x-full top-1/2 text-5xl">
+            <AiOutlineShoppingCart className="transform  -scale-x-100" />
+            {numItems > 0 && (
+              <div className="absolute text-white top-0 left-0 bg-theme-color h-5 w-5 flex items-center text-xs justify-center p-3 rounded-full">
+                {numItems}
+              </div>
+            )}
+          </div>
+        </animated.div>
+      ))}
+    </>
+  );
+};
 
-    const drawerAnimation = useSpring({
-        to: {
-          right: cartOpen ? "0%" : "-100%"
-        },
-        from: {
-          right: cartOpen ? "-100%" : "0%"
-        },
-        config: config.slow
-      })
-    
-    const overlayAnimation = useSpring({
-        to: {opacity: cartOpen ? 1 : 0},
-        from: {opacity: cartOpen ? 0 : 1},
-        config: config.slow
-    })
-    return (
-        <a.div 
-        className="fixed cursor-pointer overscroll-contain flex h-screen w-full top-0 right-0"
-        style={drawerAnimation}>
-            <a.div 
-            style={overlayAnimation} 
-            className="w-screen h-screen z-30 bg-gray-500 bg-opacity-60"
-            onClick={() => toggleDrawer()}
+const Items = ({ meals, addMealToOrder, removeMealFromOrder }) => (
+  <div className="w-full h-full flex flex-col overflow-y-auto">
+    {meals.map(({ meal, quantity }) => (
+      <CartSingleMeal
+        key={meal.id}
+        meal={meal}
+        quantity={quantity}
+        handleAddOne={addMealToOrder}
+        handleSubtractOne={removeMealFromOrder}
+      />
+    ))}
+  </div>
+);
+
+const Seperator = () => <div className="h-1 mr-2 my-5 w-1/2 bg-offwhite"></div>;
+const Total = ({ total }) => <div className="mr-2">${total}</div>;
+const CheckoutButton = ({}) => (
+  <Button text="Checkout" className="mx-auto mb-10" />
+);
+
+export const Drawer = ({ order, addMealToOrder, removeMealFromOrder }) => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const orderMap = (order && order.meals) || new Map();
+  const mealsInOrder = Array.from(orderMap.values());
+  const numItems = order.totalMeals;
+  const total = order.totalPrice;
+
+  const transitions = useTransition(cartOpen, null, {
+    from: { transform: "translate3d(100%,0,0)" },
+    enter: { transform: "translate3d(0%,0,0)" },
+    leave: { transform: "translate3d(100%,0,0)" },
+  });
+
+  return (
+    <div className="select-none">
+      {transitions.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.div
+              className="text-white bg-theme-color z-50 fixed cursor-pointer flex flex-col h-screen w-96 top-0 right-0"
+              key={key}
+              style={props}
             >
-            </a.div>
-            <div className="w-80 z-50 right-0 bg-green-400 h-full">
-                {mealsInOrder.map(({meal,quantity}) => (
-                    <CartSingleMeal 
-                    key={meal.id}
-                    meal={meal} 
-                    quantity={quantity} 
-                    handleRemove={()=>null}
-                    handleAddOne={()=>null}
-                    handleSubtractOne={()=>null}
+              <h1 className="p-3 w-full pt-10 mb-10 bg-gray-600 bg-opacity-50 text-center text-3xl">
+                Your bag
+              </h1>
+              <div className="p-5 w-full overflow-y-auto flex flex-col items-end h-full">
+                {numItems > 0 ? (
+                  <>
+                    <Items
+                      meals={mealsInOrder}
+                      addMealToOrder={addMealToOrder}
+                      removeMealFromOrder={removeMealFromOrder}
                     />
-                ))}
-            </div>
-        </a.div>
-    )
-}
+                    <Seperator />
+                    <Total total={total} />
+                    <CheckoutButton />
+                  </>
+                ) : (
+                  <p className="m-auto">Oops your cart is empty</p>
+                )}
+              </div>
+            </animated.div>
+          )
+      )}
+      <Leaf cartOpen={cartOpen} setCartOpen={setCartOpen} numItems={numItems} />
+    </div>
+  );
+};
 
 Drawer.propTypes = {
-    order: PropTypes.object.isRequired,
-}
+  order: PropTypes.object.isRequired,
+  addMealToOrder: PropTypes.func.isRequired,
+  removeMealFromOrder: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = (state) => ({
-    order: state.order.order,
-})
+  order: state.order.order,
+});
 
-export default connect(mapStateToProps, {})(Drawer)
+export default connect(mapStateToProps, {
+  addMealToOrder,
+  removeMealFromOrder,
+})(Drawer);
